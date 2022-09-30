08f53508fe913db73d959e1fdab5ed2dad98ab81
Parneet Johal <parneetkjohal@gmail.com>
2022-09-30 19:14:09 +0100

    Add if loop to find median of an array

--- median.py
+++ median.py
@@ -5,10 +5,18 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        numbers.sort()
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
 
+if len(numbers)%2 == 0:
+    mid = int(len(numbers)/2)
+    median = ((numbers[mid] + numbers[mid-1])/2)
+else:
+    mid = len(numbers) // 2
+    median = numbers[mid]
+
     # test push
-print(numbers)
+print(median)
